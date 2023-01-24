36cddc611a952fb3390ed59741fe8f6f01ba498c
Kavisara Thisakobsuk <kavisara.t@gipsic.com>
2023-01-25 00:28:41 +0700

    first main layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import logo from "./assets/images/logo.png";
 
 const App = () => {
-  return <div className="app">Good Luck</div>;
+  return (
+    <div className="app background">
+      <div className="logo">
+        <img className="imgLogo" src={logo} alt="logo" />
+      </div>
+      <div className="container">
+        <p className="title">ชื่อของคุณ</p>
+        <input />
+      </div>
+    </div>
+  );
 };
 
 export default App;
